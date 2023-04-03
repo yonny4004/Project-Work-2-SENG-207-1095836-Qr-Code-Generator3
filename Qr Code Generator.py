575ed9c9a1444751866a07c2d90bea3678d44d5a
yonny4004 <114967780+yonny4004@users.noreply.github.com>
2023-04-03 16:14:06 -0400

    Add files via upload

---
+++ Qr Code Generator.py
@@ -0,0 +1,38 @@
+import PySimpleGUI as sg 
+import os 
+import qrcode 
+
+sg.theme('Material2')
+layout=[ 
+    [sg.Input(size=(25,1),key="-WEB ADDRESS-")], 
+    [sg.Button("Generate QR Code")], 
+    [sg.Image(key="-IMAGE-",size=(300,300))] 
+] 
+window= sg.Window("QR Code Generator", layout) 
+ 
+
+def generate_qr_code(link): 
+    
+    qr=qrcode.QRCode( 
+        version=1, 
+        box_size=10, 
+        border=5 
+    ) 
+    qr.add_data(link) 
+    qr.make(fit=True) 
+    img=qr.make_image(fill="black",back_color="white") 
+    file_name="qr_code" + ".png" 
+    path = os.path.join(os.getcwd(),file_name) 
+    print(path) 
+    img.save(path) 
+    return path 
+ 
+while True: 
+    event,values=window.read() 
+    if event=="Exit" or event==sg.WIN_CLOSED: 
+        break 
+    if event=="Generate QR Code": 
+        web_address= values["-WEB ADDRESS-"] 
+        qr_code_image_path=generate_qr_code(web_address) 
+        window["-IMAGE-"].update(filename=qr_code_image_path) 
+window.close
